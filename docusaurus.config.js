9ffba5a678a6d000b0ce1e47b409844d026cd3fc
Abheek Basu <abheek94@gmail.com>
2023-06-29 19:27:44 -0400

    small changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Product Adoption example',
+  tagline: 'with laudspeaker modals',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
